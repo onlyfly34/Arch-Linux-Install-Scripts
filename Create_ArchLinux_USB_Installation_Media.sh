a8670a416d96a01d0106146abd52151e4972404b
Chi-Zer Huang <onlyfly34@gmail.com>
2016-02-13 15:05:59 +0800

    Modify shell scripts.

--- Create_ArchLinux_USB_Installation_Media.sh
+++ Create_ArchLinux_USB_Installation_Media.sh
@@ -1,16 +1,28 @@
-#Download Arch Linux iso and signature file
+#Download Arch Linux iso file
 echo -e "\033[31mDownload Arch Linux iso and signature file\033[0m\n"
-wget -r -nd -A *.iso -A *.iso.sig -l 1 -N http://archlinux.cs.nctu.edu.tw/iso/latest/
-
-#驗證
+wget -r -nd -A *.iso -l 1 -N http://archlinux.cs.nctu.edu.tw/iso/latest/
 
 #Format the USB drive
+echo -e "\033[31mFormat the USB drive and import iso to USB drive\033[0m"
 diskutil list
-read -p "Choose the USB drive you want to format:" usb_device
+xread -p "Choose the USB drive you want to format:" usb_device
 diskutil unmountDisk ${usb_device}
+
+#
+for file in `ls *.iso`
+do
+  filename=${file%.*};
+  extname=${file##*.};
+done
+dd if=${filename}.${extname} of=${usb_device} bs=8192
+echo -e "\033[32mUSB installation media is done and you can unplug it now!\033[0m"
 
 #
 
 #
 echo "Hello world test"
 echo "\nYour full name is: ${usb_device}"
+echo -e "\033[31mTest\033[0m"
+echo -e "\033[31mTest\033[0m"
+echo -e "\033[31mTest\033[0m"
+echo -e "\033[31mTest\033[0m"
